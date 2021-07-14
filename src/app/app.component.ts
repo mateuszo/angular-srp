7a4ba91a0626534826f031e2219204ef9454f42b
Mateusz Ostafil <mostafil@divante.com>
2021-07-14 14:38:52 +0200

    Add Angular SRP exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-srp';
+  users$ = this.http.get('https://jsonplaceholder.typicode.com/users/');
+
+  selectedUser$ = new BehaviorSubject(null);
+
+  selectedUserTodos$ = this.selectedUser$.pipe(
+    switchMap((user) =>
+      this.http.get(
+        `https://jsonplaceholder.typicode.com/users/${user?.id}/todos`
+      )
+    )
+  );
+
+  constructor(private http: HttpClient) {}
+
+  getUserImage(user) {
+    return `https://avatars.dicebear.com/api/avataaars/${user.username}.svg`;
+  }
 }
